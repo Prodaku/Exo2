1554a2b5c2fb7dd6113984ef71bc7ce077b87a24
Prodaku <b.isaacpro@gmail.com>
2018-12-17 15:01:20 +0100

    Fini

---
+++ FirstTP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            int total = (x + y) * z;
+            Console.WriteLine("Les 3 variables x, y et z correspondent à : " + x + ", " + y + " et " + z);
+            Console.WriteLine("Le produit de la somme de x et y par z donne " +total);
+            Console.ReadLine();
+        }
+    }
+}
